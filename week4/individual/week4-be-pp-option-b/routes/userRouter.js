af5f27d7f9037c0bdd6a529ab3e3eadc10ce0a19
Paavo <paavobro@gmail.com>
2026-02-02 19:04:29 +0200

    feat(controllers): add createUser

--- week4/individual/week4-be-pp-option-b/routes/userRouter.js
+++ week4/individual/week4-be-pp-option-b/routes/userRouter.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers } = require('../controllers/userControllers');
 
+const createUser = (req, res) => {
+   const {
+      name,
+      email,
+      password,
+      phone_number,
+      gender,
+      date_of_birth,
+      membership_status,
+   } = req.body;
+
+   const newUser = User.addOne(
+      name,
+      email,
+      password,
+      phone_number,
+      gender,
+      date_of_birth,
+      membership_status
+   );
+
+   if (!newUser) {
+      return res.status(500).json({ message: 'Fail to create user' });
+   }
+
+   res.json(newUser);
+};
+
 router.get('/', getAllUsers);
+
+router.post('/', createUser);
 
 module.exports = router;
