afd26b0d013e8489a602072ea6279bfbc93ecafa
Paavo <paavobro@gmail.com>
2026-02-02 18:49:13 +0200

    feat(models): add userModel addOne()

--- week4/individual/week4-be-pp-option-b/models/userModel.js
+++ week4/individual/week4-be-pp-option-b/models/userModel.js
@@ -5,4 +5,33 @@ const getall = () => {
     return userArray
 }
 
+const addOne = (
+    name,
+    email,
+    passowrd,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status
+) => {
+    if (!name || !email || !password || !phone_number || !gender || !date_of_birth || !membership_status) {
+      return false
+    }
+    
+    const newUser = {
+      id: nextId++,
+      name,
+      email,
+      password,
+      phone_number,
+      gender,
+      date_of_birth,
+      membership_status,
+   };
+
+   userArray.push(newUser);
+   return newUser;
+}
+
+
 module.exports = {}
