84d4150732a18d5d0c5eabf63e18407f1354b41f
Paavo <paavobro@gmail.com>
2026-02-02 19:21:30 +0200

    fix

--- week4/individual/week4-be-pp-option-b/app.js
+++ week4/individual/week4-be-pp-option-b/app.js
@@ -4,35 +4,14 @@ const app = express();
 // Middleware to parse JSON
 app.use(express.json());
 
-const tourRouter = require('./routes/tourRouter');
-app.use('/tours', tourRouter);
-
-const {
-  getAllTours,
-  getTourById,
-  createTour,
-  updateTour,
-  deleteTour,
-} = require("./controllers/tourControllers.js"); 
+const tourRouter = require("./routes/tourRouter");
+const userRouter = require("./routes/userRouter");
 
 // ROUTES
-// GET /tours
-app.get("/tours", getAllTours);
-
-// POST /tours
-app.post("/tours", createTour);
-
-// GET /tours/:tourId
-app.get("/tours/:tourId", getTourById);
-
-// PUT /tours/:tourId
-app.put("/tours/:tourId", updateTour);
-
-// DELETE /tours/:tourId
-app.delete("/tours/:tourId", deleteTour);
+app.use("/tours", tourRouter);
+app.use("/users", userRouter);
 
 const port = 4000;
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
