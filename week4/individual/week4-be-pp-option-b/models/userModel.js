6daba4ebe11c58e42432028edf4e1c7ebd65c997
Paavo <paavobro@gmail.com>
2026-02-02 18:50:35 +0200

    feat(models): add userModel updateOneById()

--- week4/individual/week4-be-pp-option-b/models/userModel.js
+++ week4/individual/week4-be-pp-option-b/models/userModel.js
@@ -38,4 +38,19 @@ const findById = (id) => {
    return user || false;
 };
 
+const updateOneById = (id, updatedData) => {
+   const user = findById(id);
+   if (!user) return false;
+
+   if (updatedData.name) user.name = updatedData.name;
+   if (updatedData.email) user.email = updatedData.email;
+   if (updatedData.password) user.password = updatedData.password;
+   if (updatedData.phone_number) user.phone_number = updatedData.phone_number;
+   if (updatedData.gender) user.gender = updatedData.gender;
+   if (updatedData.date_of_birth) user.date_of_birth = updatedData.date_of_birth;
+   if (updatedData.membership_status) user.membership_status = updatedData.membership_status;
+
+   return user;
+};
+
 module.exports = {}
